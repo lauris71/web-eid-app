61ff953bea70b5194617274f73575b26d56424d9
Lauris Kaplinski <lauris@raulwalter.com>
2023-11-15 16:09:03 +0200

    Clear pin string from dialog after query

--- src/ui/webeiddialog.cpp
+++ src/ui/webeiddialog.cpp
@@ -284,7 +284,10 @@ QString WebEidDialog::getPin()
 {
     // getPin() is called from background threads and must be thread-safe.
     // QString uses QAtomicPointer internally and is thread-safe.
-    return pin;
+    // There should be only single reference and this is transferred to the caller for safety
+    QString ret = pin;
+    pin = "";
+    return ret;
 }
 
 void WebEidDialog::onSmartCardStatusUpdate(const RetriableError status)
